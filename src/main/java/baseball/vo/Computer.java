7307f6e27af508b4555763bfef10c812a86a1499
yoondgu <doyou2ing@naver.com>
2022-11-06 12:49:02 +0900

    :recycle:refactor: Computer의 숫자 처리 메소드들을 공통 메소드로 변경 #2
    - addValidNumber, isAddable 메소드를 상위 클래스인 Numbers로 이동함.
    - 사용자의 숫자를 생성할 때에도 사용하기 위함임.
    - 컴퓨터와 사용자 간 숫자를 생성하는 로직은 서로 다르고, 숫자개수/범위/이에 따른 검증로직을 공유함.
    - 따라서 Numbers 클래스의 추상메소드를 제거하고, 추상클래스를 일반클래스로 변경함.

--- src/main/java/baseball/vo/Computer.java
+++ src/main/java/baseball/vo/Computer.java
@@ -6,8 +6,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Computer extends Numbers {
-    private static final Computer instance = new Computer();
     private Computer() { }
+    private static final Computer instance = new Computer();
     public static Computer getInstance() {
         return instance;
     }
@@ -23,27 +23,5 @@ public class Computer extends Numbers {
             int randomNumber = Randoms.pickNumberInRange(START_INCLUSIVE, END_INCLUSIVE);
             addValidNumber(numberList, randomNumber);
         }
-    }
-
-    // TODO 추후 User의 Number 기능 구현 시 Numbers 클래스로 묶을지 검토할 것.
-    private void addValidNumber(List<Integer> numberList, int number) {
-        if (isAddable(numberList, number)) {
-            numberList.add(number);
-        }
-    }
-
-    // TODO 추후 User의 Number 기능 구현 시 Numbers 클래스로 묶을지 검토할 것.
-    private boolean isAddable(List<Integer> numberList, int number) {
-        // TODO 범위 검증은 User Numbers에서만 구현하고 예외발생시킬지 검토할 것.
-        if (END_INCLUSIVE < number) {
-            return false;
-        }
-        if (START_INCLUSIVE > number) {
-            return false;
-        }
-        if (numberList.contains(number)) {
-            return false;
-        }
-        return true;
     }
 }
--- src/main/java/baseball/vo/Numbers.java
+++ src/main/java/baseball/vo/Numbers.java
@@ -2,11 +2,28 @@ package baseball.vo;
 
 import java.util.List;
 
-abstract class Numbers {
+public class Numbers {
     static final int NUMBER_COUNT = 3;
     static final int START_INCLUSIVE = 1;
     static final int END_INCLUSIVE = 9;
-//    List<Integer> numberList;
 
-    abstract List<Integer> generateNumberList();
+    void addValidNumber(List<Integer> numberList, int number) {
+        if (isAddable(numberList, number)) {
+            numberList.add(number);
+        }
+    }
+
+    boolean isAddable(List<Integer> numberList, int number) {
+        // TODO 범위 검증은 User Numbers에서만 구현하고 예외발생시킬지 검토할 것.
+        if (END_INCLUSIVE < number) {
+            throw new IllegalArgumentException("[오류] 숫자는 " + END_INCLUSIVE + "보다 클 수 없습니다.");
+        }
+        if (START_INCLUSIVE > number) {
+            throw new IllegalArgumentException("[오류] 숫자는 " + START_INCLUSIVE + "보다 작을 수 없습니다.");
+        }
+        if (numberList.contains(number)) {
+            throw new IllegalArgumentException("[오류] 모든 숫자는 서로 같을 수 없습니다.");
+        }
+        return true;
+    }
 }
