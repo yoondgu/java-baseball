64ed6e8085c83f316970f1fa0300e30f0f72bccc
yoondgu <doyou2ing@naver.com>
2022-12-16 10:00:00 +0900

    :sparkles:feat(project): 전체 클래스 구조 뼈대 작성

---
+++ src/main/java/baseball/controller/GameController.java
@@ -0,0 +1,15 @@
+package baseball.controller;
+
+import baseball.model.BaseballGame;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
+public class GameController {
+    private final BaseballGame baseballGame = new BaseballGame();
+    private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
+
+    public void run() {
+
+    }
+}
